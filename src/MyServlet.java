ac4862374388c5cc155ccbadc2f7a886c28063e6
Sivakumar B <Sivakumar.balaramasubramanian@wipro.com>
2016-10-17 16:36:03 +0530

    Sample commit

---
+++ src/MyServlet.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//sample text
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+
+		String user = request.getParameter("userName");
+		String pass = request.getParameter("userPassword");
+
+		String nextJSP = "/jsp/display.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+		dispatcher.forward(request, response);
+		
+		/*if (user.equals("java4s") && pass.equals("java4s")) {
+			String nextJSP = "/jsp/display.jsp";
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+			dispatcher.forward(request, response);
+		} else {
+			out.println("Login Failed...!");
+			out.close();
+		}*/
+
+	}
+
+}
